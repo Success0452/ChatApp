93b2cff1b9c19080ac1cc10806b4a301ed73dcf4
Success0452 <olatundesuccess54@gmail.com>
2021-06-10 11:02:12 +0100

    update

--- app/src/main/java/com/famous/chatapp/MainActivity.kt
+++ app/src/main/java/com/famous/chatapp/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.famous.chatapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
+import com.famous.chatapp.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 
@@ -10,9 +14,65 @@ class MainActivity : AppCompatActivity() {
     private var auth: FirebaseAuth? = null
     private  var firebaseUser: FirebaseUser? = null
 
-
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+        //firebaseUser = auth!!.currentUser!!
+
+        //check if user login then navigate to user screen
+        if (firebaseUser != null) {
+            val intent = Intent(
+                this@MainActivity,
+                UsersActivity::class.java
+            )
+            startActivity(intent)
+            finish()
+        }
+
+        binding.btnLogin.setOnClickListener {
+            val email = binding.etEmail.text.toString()
+            val password = binding.etPassword.text.toString()
+
+            if (TextUtils.isEmpty(email) && TextUtils.isEmpty(password)) {
+                Toast.makeText(
+                    applicationContext,
+                    "email and password are required",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                auth!!.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this) {
+                        if (it.isSuccessful) {
+                            binding.etEmail.setText("")
+                            binding.etPassword.setText("")
+                            val intent = Intent(
+                                this@MainActivity,
+                                UsersActivity::class.java
+                            )
+                            startActivity(intent)
+                            finish()
+                        } else {
+                            Toast.makeText(
+                                applicationContext,
+                                "email or password invalid",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+            }
+        }
+
+        binding.btnSignUp.setOnClickListener {
+            val intent = Intent(
+                this@MainActivity,
+                SignUpActivity::class.java
+            )
+            startActivity(intent)
+            finish()
+        }
     }
 }
